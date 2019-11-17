d0d9a0bd0662c11b7189867876aac2e205740f2d
Yes <46545975+pyrevoid15@users.noreply.github.com>
2019-11-17 09:35:26 -0500

    rivers

--- map.py
+++ map.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import math
+import time
 
 
 VIEWSCALE = 1
@@ -25,6 +26,9 @@ class Map:
             for _ in range(0, self.columns):
                 row.append(0)
             self.elevations.append(row)
+
+        print(random.getstate()[0])
+        random.seed(11111111111)
 
         self.seed()
         self.expand()
@@ -264,11 +268,21 @@ class Map:
                             except IndexError:
                                 print('Error: ', spot)
 
-        '''for y in range(0, self.rows):
+        immunity = []
+        for y in range(0, self.rows):
             for x in range(0, self.columns):
-                if self.elevations[y][x] < 0.6 and self.checkwaterlocked()'''
+                try:
+                    if self.elevations[y][x] > random.randint(35, 45) / 10 and\
+                            self.elevations[y][x] > max(self.elevations[y - 1][x], self.elevations[y + 1][x],
+                                                        self.elevations[x - 1][y], self.elevations[x + 1][y]):
+                        self.peaks.append([x, y])
+                        self.elevations[y][x] **= 1.4
+                except IndexError:
+                    pass
+                if 1.6 < self.elevations[y][x] < 2.6:
+                    self.elevations[y][x] **= 0.86
 
-        for _ in range(0, 3):
+        for p in range(0, 4):
             self.columns *= 2
             small_elev = self.elevations
             self.elevations = []
@@ -279,10 +293,16 @@ class Map:
                     if x % 2 == 0:
                         row.append(small_elev[y][int(x / 2)])
                     else:
-                        try:
-                            row.append((small_elev[y][int((x - x % 2) / 2)] + small_elev[y][int((x - x % 2) / 2) + 1]) / 20 * random.randint(8, 12))
-                        except IndexError:
-                            row.append((small_elev[y][int((x - x % 2) / 2)] + small_elev[y][0]) / 10 * random.randint(8, 12))
+                        if p < random.choice([3, 4, 4, 2, 4, 4, 3, 4, 4]):
+                            try:
+                                row.append((small_elev[y][int((x - x % 2) / 2)] + small_elev[y][int((x - x % 2) / 2) + 1]) / 20 * random.randint(8, 12))
+                            except IndexError:
+                                row.append((small_elev[y][int((x - x % 2) / 2)] + small_elev[y][0] / 10 * random.randint(8, 12)))
+                        else:
+                            try:
+                                row.append((small_elev[y][int((x - x % 2) / 2)] + small_elev[y][int((x - x % 2) / 2) + 1]) / 200 * random.randint(95, 105))
+                            except IndexError:
+                                row.append((small_elev[y][int((x - x % 2) / 2)] + small_elev[y][0]) / 100 * random.randint(95, 105))
                 fullrows.append(row)
 
             self.rows *= 2
@@ -292,15 +312,16 @@ class Map:
                 else:
                     row = []
                     for x in range(0, self.columns):
-                        try:
-                            vertavg = (fullrows[int((y - y % 2) / 2)][x] + fullrows[int((y - y % 2) / 2) + 1][x]) / 20 * random.randint(8, 12)
-                        except IndexError:
-                            vertavg = fullrows[int((y - y % 2) / 2)][x]
-
-                        '''try:
-                            horizavg = (fullrows[int((y - y % 2) / 2)][x] + fullrows[int((y - y % 2) / 2) + 1][x]) / 2
-                        except IndexError:
-                            horizavg = fullrows[int((y - y % 2) / 2)][x]'''
+                        if p < random.choice([3, 4, 4, 2, 4, 4, 3, 4, 4]):
+                            try:
+                                vertavg = (fullrows[int((y - y % 2) / 2)][x] + fullrows[int((y - y % 2) / 2) + 1][x]) / 20 * random.randint(8, 12)
+                            except IndexError:
+                                vertavg = fullrows[int((y - y % 2) / 2)][x] / 10 * random.randint(8, 12)
+                        else:
+                            try:
+                                vertavg = (fullrows[int((y - y % 2) / 2)][x] + fullrows[int((y - y % 2) / 2) + 1][x]) / 200 * random.randint(95, 105)
+                            except IndexError:
+                                vertavg = fullrows[int((y - y % 2) / 2)][x] / 100 * random.randint(95, 105)
 
                         row.append(vertavg)
                     self.elevations.append(row)
@@ -310,35 +331,123 @@ class Map:
                 if self.elevations[y][x] > 4:
                     self.elevations[y][x] **= 0.8 - (int(self.elevations[y][x] - 4) / max(22, (
                             (self.elevations[y][x] - 2) * 9) - random.randint(-3, 3))) * 1.1
+                if [x, y] in self.peaks:
+                    self.elevations[y][x] **= 1.5
         print(len(self.elevations[0]), len(self.elevations))
 
+        for n, row in enumerate(self.elevations):
+            for n2, tile in enumerate(row):
+                try:
+                    if tile > 4:
+                        tile **= 0.8 - (int(tile - 4) / max(22, (
+                                (tile - 2) * 9) - random.randint(-3, 3))) * 1.1
+                    if tile > 1:
+                        if random.randint(0, 100) < 13:
+                            tile **= 0.14
+                        else:
+                            if tile > 1.5:
+                                tile = math.sqrt(tile ** 1.1)
+                                if tile > 8 and self.tilecount > 20000:
+                                    tile **= 0.6
+                                if tile > 4:
+                                    tile **= 1.1
+                            else:
+                                try:
+                                    if (self.elevations[n][n2 - 1] > 8.7 and self.elevations[n][n2 + 1] > 8.7) or \
+                                            (self.elevations[n - 1][n2] > 8.7 and self.elevations[n + 1][n2] > 8.7):
+                                        pass
+                                except IndexError:
+                                    tile = random.randint(7, 9)
+                except:
+                    self.elevations[n][n2] = 0
+
+                if type(tile) is not float:
+                    self.elevations[n][n2] = 0
+
+        for p in self.peaks:
+            p[0] *= 16
+            p[1] *= 16
+            print(p)
+            if p[0] < 0:
+                p[0] += self.columns
+            elif p[0] >= self.columns:
+                p[0] -= self.columns
+            if p[1] < 0:
+                p[1] += self.rows
+            elif p[1] >= self.rows:
+                p[1] -= self.rows
+            p[0] %= self.columns
+            p[1] %= self.rows
+            print("~", p)
+
+            if self.elevations[p[1]][p[0]] is float:
+                if self.elevations[p[1]][p[0]] > 0:
+                    self.elevations[p[1]][p[0]] **= 1.3
+
+        for _ in range(0, int(random.randint(7, 15) / 100 * self.tilecount)):
+            loca = [0, 0]
+            while True:
+                loca = random.choice(self.peaks)
+                loca[0] += random.randint(1, 6) * random.choice([-1, 1])
+                loca[1] += random.randint(1, 6) * random.choice([-1, 1])
+
+                if not([loca[0] - 1, loca[1] - 1] in self.rivers or [loca[0] - 1, loca[1]] in self.rivers or
+                        [loca[0] - 1, loca[1] + 1] in self.rivers or [loca[0], loca[1] - 1] in self.rivers or
+                        [loca[0], loca[1] + 1] in self.rivers or [loca[0] + 1, loca[1] + 1] in self.rivers or
+                        [loca[0] + 1, loca[1] - 1] in self.rivers or [loca[0] + 1, loca[1]] in self.rivers):
+                    try:
+                        if self.elevations[loca[1]][loca[0]] > 2 and 0 < loca[0] < self.columns and 0 < loca[1] < self.rows:
+                            self.rivers.append([loca[0], loca[1]])
+                            self.loca = loca
+                            break
+                    except:
+                        try:
+                            self.elevations[loca[1]][loca[0]] = 0
+                        except:
+                            pass
+
+            flow = self.loca
+            prevflow = []
+            while True:
+                all_adj =[self.elevations[flow[1] - 1][flow[0] - 1], self.elevations[flow[1] - 1][flow[0]], self.elevations[flow[1] - 1][flow[0] + 1],
+                          self.elevations[flow[1]][flow[0] - 1], self.elevations[flow[1]][flow[0]], self.elevations[flow[1]][flow[0] + 1],
+                          self.elevations[flow[1] + 1][flow[0] - 1], self.elevations[flow[1] + 1][flow[0]], self.elevations[flow[1] + 1][flow[0] + 1]]
+                for c, a in enumerate(all_adj):
+                    if a == min(all_adj):
+                        if [int(c / 3) - 1 + flow[0], int(c % 3) - 1 + flow[1]] == flow or\
+                                [int(c / 3) - 1 + flow[0], int(c % 3) - 1 + flow[1]] in prevflow:
+                            all_adj.remove(a)
+                            # self.rivers.append([int(c / 3) - 1 + flow[0], int(c % 3) - 1 + flow[1]])
+                        else:
+                            flow = [int(c / 3) - 1 + flow[0], int(c % 3) - 1 + flow[1]]
+
+                if flow not in self.rivers:
+                    self.rivers.append(flow)
+                    prevflow.append(flow)
+                    if self.elevations[flow[1]][flow[0]] < 1:
+                        break
+                else:
+                    break
+
+        print(self.rivers)
         abi = ''
         for n, row in enumerate(self.elevations):
             abi += '|'
             for n2, tile in enumerate(row):
-                # print('(%s, %s)' % (n2, n))
-                if tile > 1:
-                    if random.randint(0, 100) < 13:
-                        tile **= 0.15
+                if [n2, n] in self.rivers:
+                    abi += 'v|'
+                elif tile > 1:
+                    if int(tile) < 9:
                         abi += str(int(tile)) + '|'
                     else:
-                        if tile > 1.5:
-                            tile = math.sqrt(tile ** 1.1)
-                            if tile > 8 and self.tilecount > 20000:
-                                tile = tile ** 0.6
-                        if int(tile) < 9:
+                        try:
+                            if (self.elevations[n][n2 - 1] > 8.7 and self.elevations[n][n2 + 1] > 8.7) or \
+                                    (self.elevations[n - 1][n2] > 8.7 and self.elevations[n + 1][n2] > 8.7):
+                                abi += '_|'
+                            else:
+                                abi += '!|'
+                        except IndexError:
                             abi += str(int(tile)) + '|'
-                        else:
-                            try:
-                                if (self.elevations[n][n2 - 1] > 8.7 and self.elevations[n][n2 + 1] > 8.7) or \
-                                        (self.elevations[n - 1][n2] > 8.7 and self.elevations[n + 1][n2] > 8.7):
-                                    # tile = 0.4
-                                    abi += '_|'
-                                else:
-                                    abi += '!|'
-                            except IndexError:
-                                tile = random.randint(7, 9)
-                                abi += str(int(tile)) + '|'
                 else:
                     abi += '_|'
             abi += '\n'
@@ -347,33 +456,52 @@ class Map:
         del islandcount, islands, small_elev, fullrows, abi
 
     def view(self):
-        self.scr = pygame.display.set_mode((self.columns * VIEWSCALE, self.rows * VIEWSCALE))
+        self.scr = pygame.display.set_mode((int(self.columns * VIEWSCALE), int(self.rows * VIEWSCALE)))
         pygame.init()
         pygame.display.set_caption("Mapview")
         viewing = True
+        self.scr.fill((255, 255, 255))
+        slider = 0
+        self.drivf = []
 
-        for y in range(0, self.rows):
-            for x in range(0, self.columns):
+        for r in self.rivers:
+            print(r)
+        print("---------------------")
+        for y in range(0, int(len(self.elevations))):
+            for x in range(0, int(len(self.elevations[0]))):
+                a = pygame.Surface((VIEWSCALE, VIEWSCALE))
+                self.drivf = pygame.mouse.get_pos()
                 pygame.event.get()
-                tl = pygame.Surface((VIEWSCALE, VIEWSCALE))
-                vl = [x, y]
-                if self.elevations[y][x] > 1:
-                    color = [max(0, min(255, int(self.elevations[y][x] / 11 * 255))),
-                             0, max(0, min(255, int(self.elevations[y][x] / 11 * 255)))]
+                el = self.elevations[y][x]
+                if [x, y] in self.rivers:
+                    color = [0, 255, 255]
+                    print([x, y])
+                elif el > 1:
+                    color = [max(0, min(255, int(el / 11 * 255))),
+                             0, max(0, min(255, int(el / 11 * 255)))]
                 else:
-                    color = [0, 0, 0]
+                    color = [255, 255, 255]
 
-                tl.fill(color)
-                print("#", color)
-                self.scr.blit(tl, (x * VIEWSCALE, y * VIEWSCALE))
-                pygame.display.flip()
+                a.fill(color)
+                self.scr.blit(a, (x * VIEWSCALE, y * VIEWSCALE))
+                # pygame.display.flip()
 
+        print(len(self.peaks))
+        for p in self.peaks:
+            print(p)
         while viewing:
-            print("the ye")
+            self.drivf = pygame.mouse.get_pos()
             for e in pygame.event.get():
                 if e.type == pygame.QUIT:
                     sys.exit()
 
+            pygame.display.set_caption(str(((self.drivf[0] - self.drivf[0] % VIEWSCALE) / VIEWSCALE,
+                                            (self.drivf[1] - self.drivf[1] % VIEWSCALE) / VIEWSCALE)) +
+                                       str(self.elevations[int((self.drivf[1] - self.drivf[1] % VIEWSCALE) / VIEWSCALE)]
+                                           [int((self.drivf[0] - self.drivf[0] % VIEWSCALE) / VIEWSCALE)]))
             pygame.display.flip()
+            time.sleep(0.1)
 
-g = Map()
+
+if __name__ == "__main__":
+    g = Map()
